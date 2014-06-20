ac740d6a289b94a1bad1c0cd9e66392523315b6d
Jared Kuemmerle <jared.kuemmerle@yahoo.com>
2014-06-20 15:02:32 -0700

    implement partials

--- mainapp/static/mainapp/js/main.js
+++ mainapp/static/mainapp/js/main.js
@@ -1,6 +1,36 @@
-angular.module('fundfab', ['ui.bootstrap'])
+angular.module('fundfab', ['ui.bootstrap', 'ui.router']).
+    config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+    $urlRouterProvider.otherwise("/");
+    $stateProvider
+      .state('home',{
+        url: '/',
+        views: {
+            'header': {
+                template: '<div>header</div>',
+                controller: headerCtrl
+            },
+            'content': {
+                templateUrl: '/static/fundfab/templates/home.content.html',
+                controller: ListingsCtrl
+            },
+            'footer': {
+                template: '<div>footer</div>',
+                controller: footerCtrl
+            },
+        }
+    });
+    // @todo html5mode $locationProvider.html5mode(true);
+});
 
 function ListingsCtrl($scope) {
+
+    $scope.title = 'My Custom Title';
+}
+function headerCtrl($scope) {
+
+    $scope.title = 'My Custom Title';
+}
+function footerCtrl($scope) {
 
     $scope.title = 'My Custom Title';
 }
