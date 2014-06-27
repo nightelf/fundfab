df263c84396d14237116f23fc03fd7fc85ff019a
Jared Kuemmerle <jared.kuemmerle@yahoo.com>
2014-06-27 08:24:50 -0700

    move headers and footers to ng-include for more permanence.

--- mainapp/static/mainapp/js/main.js
+++ mainapp/static/mainapp/js/main.js
@@ -1,4 +1,4 @@
-angular.module('fundfab', ['ui.bootstrap', 'ui.router']).
+var fundfab = angular.module('fundfab', ['ui.bootstrap', 'ui.router']).
     config(function($stateProvider, $urlRouterProvider, $locationProvider) {
     $urlRouterProvider
         .when('', '/')
@@ -7,32 +7,26 @@ angular.module('fundfab', ['ui.bootstrap', 'ui.router']).
       .state('home',{
         url: '/',
         views: {
-            'header': {
-                templateUrl: '/static/fundfab/templates/home.header.html',
-                controller: headerCtrl
-            },
             'content': {
                 templateUrl: '/static/fundfab/templates/home.content.html',
                 controller: ListingsCtrl
-            },
-            'footer': {
-                templateUrl: '/static/fundfab/templates/home.footer.html',
-                controller: footerCtrl
             },
         }
     });
     // @todo html5mode $locationProvider.html5mode(true);
 });
 
+
 function ListingsCtrl($scope) {
 
     $scope.title = 'My Custom Title';
 }
-function headerCtrl($scope) {
 
+fundfab.controller('headerCtrl', ['$scope', function($scope) {
     $scope.title = 'My Custom header';
-}
-function footerCtrl($scope) {
+}]);
+
+fundfab.controller('footerCtrl', ['$scope', function($scope) {
 
     $scope.title = 'My Custom footer';
-}
+}]);
