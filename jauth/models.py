8f98f333802953f93097df306562aedb72fcd821
Jared Kuemmerle <jared.kuemmerle@yahoo.com>
2014-07-15 15:43:52 -0700

    add superuser role to create admin

--- jauth/models.py
+++ jauth/models.py
@@ -31,6 +31,7 @@ class JUserManager(BaseUserManager):
         """
         user = self.create_user(email, password=password)
         user.is_admin = True
+        user.is_superuser = True
         user.save(using=self._db)
         return user
 
--- jauth/urls.py
+++ jauth/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'login-email-submit', 'jauth.views.login_email_submit'),
