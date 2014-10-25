55a921b4b624b34865f26da44d5f0fc4535dc34c
Jared Kuemmerle <jared.kuemmerle@yahoo.com>
2014-10-24 23:02:38 -0700

    remove extraneious function from view.

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.contrib.auth.models import User
 # Create your views here.
 
 def index(request):
